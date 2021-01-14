e0a54cd9c8d15c463ed5c490ca47f6308d72d5a5
gieeedreee <66144591+gieeedreee@users.noreply.github.com>
2021-01-14 12:33:25 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='tesla',
+    version='1.1',
+    packages=['tesla'],
+    url='https://github.com/gieeedreee/tesla',
+    license='',
+    author='Giedre',
+    author_email='gieeedreee@gmail.com',
+    description='package'
+)
