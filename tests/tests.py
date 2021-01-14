367e93b946890b55bfef377bfe70de0cecc736a2
gieeedreee <66144591+gieeedreee@users.noreply.github.com>
2021-01-14 02:03:40 +0200

    Add files via upload

---
+++ tests/tests.py
@@ -0,0 +1,14 @@
+from tesla.fabric import Tesla
+
+
+def test_drive():
+    model7 = Tesla('Model7', 'green', True, 0.3)
+    travel_range = 100
+    result = Tesla.drive(model7, travel_range)
+    assert result == "Battery charge level is 69.9%"
+
+
+def test_is_locked():
+    model5 = Tesla('Model5', 'white', True, 0.2)
+    result = Tesla.lock(model5)
+    assert result == False
